f0c72a9c36d6567d42ce605f0a6c876e46dc72cb
swiftmas <collinhamel@gmail.com>
2018-06-24 00:44:23 -0700

    added audio tests. Got audio syncched and running and added buffer class

--- kek-rr/main.js
+++ kek-rr/main.js
@@ -5,3 +5,38 @@ socket.on('data', function(newdata) {
 	document.getElementById('chatText').innerHTML=data
 	document.getElementById('usrmsg').value = "";
 });
+
+
+window.onload = init;
+var context;
+var bufferLoader;
+
+function init() {
+  // Fix up prefixing
+  window.AudioContext = window.AudioContext || window.webkitAudioContext;
+  context = new AudioContext();
+
+  bufferLoader = new BufferLoader(
+    context,
+    [
+      '/data/1.wav',
+      '/data/2.wav',
+    ],
+    finishedLoading
+    );
+
+  bufferLoader.load();
+}
+
+function finishedLoading(bufferList) {
+  // Create two sources and play them both together.
+  var source1 = context.createBufferSource();
+  var source2 = context.createBufferSource();
+  source1.buffer = bufferList[0];
+  source2.buffer = bufferList[1];
+
+  source1.connect(context.destination);
+  source2.connect(context.destination);
+  source1.start(0);
+  source2.start(0);
+}
