66776096142276573f6e7eae813444565ec7b0b3
nemosaurus <nemosaurus@gmail.com>
2018-06-24 19:42:42 -0700

    visualizer

--- kek-rr/main.js
+++ kek-rr/main.js
@@ -14,8 +14,29 @@ var playing = false;
 
 function init() {
   // Fix up prefixing
+  //Wire it up
   window.AudioContext = window.AudioContext || window.webkitAudioContext;
   context = new AudioContext();
+  analyser = context.createAnalyser();
+  source1 = context.createBufferSource();
+  source2 = context.createBufferSource();
+  source1.connect(analyser);
+  // source2.connect(analyser);
+  source1.connect(context.destination);
+  source2.connect(context.destination);
+
+  analyser.fftSize = 2048;
+  bufferLength = analyser.frequencyBinCount;
+  dataArray = new Uint8Array(bufferLength);
+  analyser.getByteTimeDomainData(dataArray);
+
+  canvas=document.getElementById("visi");
+  canvasCtx=canvas.getContext("2d");
+  WIDTH = 500;
+  HEIGHT = 300;
+  canvasCtx.fillRect(0, 0, 300, 150);
+  // canvasCtx.clearRect(0, 0, WIDTH, HEIGHT);
+
 
   bufferLoader = new BufferLoader(
     context,
@@ -39,100 +60,49 @@ function flip(){
 
 function finishedLoading(bufferList) {
   // Create two sources and play them both together.
-  var source1 = context.createBufferSource();
-  var source2 = context.createBufferSource();
   source1.buffer = bufferList[0];
   source2.buffer = bufferList[1];
-
-  source1.connect(context.destination);
-  source2.connect(context.destination);
   source1.start(0);
   source2.start(0);
-}
-var canvas = document.createElement('canvas');
+  draw();
 
-canvas.id = "CursorLayer";
-canvas.width = 1224;
-canvas.height = 768;
-canvas.style.zIndex = 8;
-canvas.style.position = "absolute";
-canvas.style.border = "1px solid";
-
-
-var body = document.getElementsByTagName("body")[0];
-
-
-// AUDIO CONTEXT
-window.AudioContext = window.AudioContext || window.webkitAudioContext ;
-
-if (!AudioContext) alert('This site cannot be run in your Browser. Try a recent Chrome or Firefox. ');
-
-var audioContext = new AudioContext();
-var currentBuffer  = null;
-
-// CANVAS
-var canvasWidth = 512,  canvasHeight = 120 ;
-var newCanvas   = createCanvas (canvasWidth, canvasHeight);
-var context     = null;
-
-window.onload = appendCanvas;
-function appendCanvas() { document.body.appendChild(newCanvas);
-                          context = newCanvas.getContext('2d'); }
-
-// MUSIC LOADER + DECODE
-function loadMusic(url) {   
-    var req = new XMLHttpRequest();
-    req.open( "GET", url, true );
-    req.responseType = "arraybuffer";    
-    req.onreadystatechange = function (e) {
-          if (req.readyState == 4) {
-             if(req.status == 200)
-                  audioContext.decodeAudioData(req.response, 
-                    function(buffer) {
-                             currentBuffer = buffer;
-                             displayBuffer(buffer);
-                    }, onDecodeError);
-             else
-                  alert('error during the load.Wrong url or cross origin issue');
-          }
-    } ;
-    req.send();
 }
 
-function onDecodeError() {  alert('error while decoding your file.');  }
+function draw() {
+  drawVisual = requestAnimationFrame(draw);
+  analyser.getByteTimeDomainData(dataArray);
 
-// MUSIC DISPLAY
-function displayBuffer(buff /* is an AudioBuffer */) {
-   var leftChannel = buff.getChannelData(0); // Float32Array describing left channel     
-   var lineOpacity = canvasWidth / leftChannel.length  ;      
-   context.save();
-   context.fillStyle = '#222' ;
-   context.fillRect(0,0,canvasWidth,canvasHeight );
-   context.strokeStyle = '#121';
-   context.globalCompositeOperation = 'lighter';
-   context.translate(0,canvasHeight / 2);
-   context.globalAlpha = 0.06 ; // lineOpacity ;
-   for (var i=0; i<  leftChannel.length; i++) {
-       // on which line do we get ?
-       var x = Math.floor ( canvasWidth * i / leftChannel.length ) ;
-       var y = leftChannel[i] * canvasHeight / 2 ;
-       context.beginPath();
-       context.moveTo( x  , 0 );
-       context.lineTo( x+1, y );
-       context.stroke();
-   }
-   context.restore();
-   console.log('done');
-}
+  canvasCtx.fillStyle = 'rgb(200, 200, 200)';
+  canvasCtx.fillRect(0, 0, WIDTH, HEIGHT);
 
-function createCanvas ( w, h ) {
-    var newCanvas = document.createElement('canvas');
-    newCanvas.width  = w;     newCanvas.height = h;
-    return newCanvas;
+  canvasCtx.lineWidth = 2;
+  canvasCtx.strokeStyle = 'rgb(0, 0, 0)';
+
+  canvasCtx.beginPath();
+
+  var sliceWidth = WIDTH * 1.0 / bufferLength;
+  var x = 0;
+
+  for(var i = 0; i < bufferLength; i++) {
+
+    var v = dataArray[i] / 128.0;
+    var y = v * HEIGHT/2;
+
+    if(i === 0) {
+      canvasCtx.moveTo(x, y);
+    } else {
+      canvasCtx.lineTo(x, y);
+    }
+
+    x += sliceWidth;
+  }
+  canvasCtx.lineTo(canvas.width, canvas.height/2);
+  canvasCtx.stroke();
 };
 
 
-// loadMusic('could_be_better.mp3');
 
 
-loadMusic('http://localhost:3030/data/1.wav');
+
+
+
