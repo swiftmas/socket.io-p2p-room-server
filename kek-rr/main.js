4dd0df5d38a0d880bee5bb87f24b2fabf8e7becb
swiftmas <collinhamel@gmail.com>
2018-07-08 16:50:29 -0700

    started implimenting an elementary selector

--- kek-rr/main.js
+++ kek-rr/main.js
@@ -19,7 +19,7 @@ window.onload = init;
 function init() {
   window.AudioContext = window.AudioContext || window.webkitAudioContext;
   context = new AudioContext();
-	loadKek("/data/song1.kek", "url")
+	loadKek("/data/newSong.kek", "url")
 }
 
 
@@ -31,70 +31,20 @@ function mainLoop(){
 	}, 60/songData.bpm/4);
 }
 
-function newTrack(track, file){
+function newAudio(track, file, measure, beat){
     kekFileData[file.name] = {}
     kekFileData[file.name].value = file;
     createBuffer(file.name);
   	var rev = songData.tracks[track].currentRevision
-  	var newtrack = {"file":file.name, "init_formal": [0,0,0,0,0]}
+  	var newtrack = {"file":file.name, "init_formal": [measure,beat,0,0,0]}
   	songData.tracks[track].revisions[rev].audio.push(newtrack)
     finishedLoading()
 }
 
-
-function pageDraw(){
-  console.log("DrawTracks")
-  let htmlData = ""
-  for (var track=0; track<songData.tracks.length; ++track) {
-    let trackName = songData.tracks[track].trackName
-    let mark1 = `
-        <div class="track-header">
-          ${trackName} <br>
-          <select onchange="changeRev(this, ${track})">
-        `
-    let mark2 = ""
-    for (var revision in songData.tracks[track].revisions){
-      mark2 +=`<option value="${revision}">${revision}</option>`
-    }
-    let mark3 =`
-          </select>
-        </div>
-        `
-    htmlData = htmlData + mark1 + mark2 + mark3
-  }
-  document.getElementById('trackHeaders').innerHTML = htmlData
-
-}
-
-
-function changeRev(revValue, track){
-    stop()
-    var value = revValue.value;
-    songData.tracks[track].currentRevision = value
-}
-
-function draw(){
-	var zoom = 16
-	canvas.width = songData.end * zoom
-	canvas.height = songData.tracks.length * 82
-	for (var i=0; i<songData.tracks.length; ++i) {
-		var rev = songData.tracks[i].currentRevision
-		for (var af=0; af<songData.tracks[i].revisions[rev].audio.length; ++af){
-			var timeTotal = songData.tracks[i].revisions[rev].audio[af].init_formal
-			var tlen = globalTime("time", bufferData.buffers[songData.tracks[i].revisions[rev].audio[af].file].duration, "formal")
-			var my_gradient = ctx.createLinearGradient(0, 0, 0, 170+(100*i));
-			my_gradient.addColorStop(0, "#2ff3f3");
-			my_gradient.addColorStop(1, "#b400e1");
-      //my_gradient.addColorStop(0, "#eee");
-			//my_gradient.addColorStop(1, "#fff");
-			ctx.fillStyle = my_gradient;
-			ctx.fillRect(timeTotal[0]*zoom,i*82,tlen[0]*zoom,80);
-			ctx.fillStyle = "Black"
-			var newtime = globalTime("time",(context.currentTime - playTime),"formal")
-			ctx.fillRect((newtime[0]*zoom)+parseInt(newtime[1]*(zoom/4))+parseInt(newtime[2]*(zoom/16)),0,1,82*songData.tracks.length)
-			document.getElementById('currentTime').innerHTML = "Measure: " + newtime[0] + " , Quarter: " + newtime[1] + " , 16th: " + newtime[2]
-		}
-	}
+function newTrack(){
+  let newtrack = {"trackName":"New","currentRevision":"Master","revisions":{"Master":{"settings":{"volume":100},"effects":[{"effectName":"Reverb","amount":10}],"audio":[]}}}
+  songData.tracks.push(newtrack);
+  pageDraw();
 }
 
 
