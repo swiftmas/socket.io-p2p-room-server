4b4cc339feac6dfaa658c9667541e94edb47ffdd
Collin Hamel <chamel@MAC-CHAMEL.domain>
2018-07-14 08:08:44 -0700

    started on record

--- kek-rr/main.js
+++ kek-rr/main.js
@@ -10,6 +10,7 @@ var chatData = "";
 var socket = io.connect();
 var userSocket;
 var roomId;
+var recorder
 var opts = {peerOpts: {trickle: false}, autoUpgrade: false}
 var canvas = document.getElementById('canvaz');
 var ctx = canvas.getContext('2d');
@@ -29,6 +30,19 @@ function init() {
   window.AudioContext = window.AudioContext || window.webkitAudioContext;
   context = new AudioContext();
 	loadKek("/data/"+ songName, "url")
+  navigator.mediaDevices.getUserMedia({ audio: true })
+    .then(stream => {
+      recorder = new MediaRecorder(stream);
+      recorder.addEventListener("dataavailable", event => {
+            console.log("its workings")
+            kekFileData["record"]=[]
+            kekFileData["record"].push(event.data)
+            kekFileData["newAudio"] = new Blob(kekFileData["record"])
+            //.then(function(){
+            //  bufferData(newAudio)
+            //});
+      });
+    });
 }
 
 
@@ -39,6 +53,71 @@ function mainLoop(){
 		draw()
 	}, 60/songData.bpm/4);
 }
+
+function recordAudio() {
+    // start recording using Recorder.js
+    //recorder.clear();
+    //recorder.startTime = context.currentTime;
+    recorder.start();
+}
+
+
+
+function recordStop() {
+    // stop recording and get the recorded buffer data
+    recorder.stop();
+}
+
+
+
+
+function exportWAV(type, before, after){
+    var rate = 22050;
+    if (!before) { before = 0; }
+    if (!after) { after = 0; }
+
+    var channel = 0,
+        buffers = [];
+    for (channel = 0; channel < numChannels; channel++){
+        buffers.push(mergeBuffers(recBuffers[channel], recLength));
+    }
+
+    var i = 0,
+        offset = 0,
+        newbuffers = [];
+
+    for (channel = 0; channel < numChannels; channel += 1) {
+        offset = 0;
+        newbuffers[channel] = new Float32Array(before + recLength + after);
+        if (before > 0) {
+            for (i = 0; i < before; i += 1) {
+                newbuffers[channel].set([0], offset);
+                offset += 1;
+            }
+        }
+        newbuffers[channel].set(buffers[channel], offset);
+        offset += buffers[channel].length;
+        if (after > 0) {
+            for (i = 0; i < after; i += 1) {
+                newbuffers[channel].set([0], offset);
+                offset += 1;
+            }
+        }
+    }
+
+    if (numChannels === 2){
+        var interleaved = interleave(newbuffers[0], newbuffers[1]);
+    } else {
+        var interleaved = newbuffers[0];
+    }
+
+    var downsampledBuffer = downsampleBuffer(interleaved, rate);
+    var dataview = encodeWAV(downsampledBuffer, rate);
+    var audioBlob = new Blob([dataview], { type: type });
+
+    this.postMessage(audioBlob);
+}
+
 
 function newAudio(file, measure, beat){
     kekFileData[file.name] = {}
